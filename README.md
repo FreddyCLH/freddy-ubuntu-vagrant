72a1c689999ab113977f2e10729017d67202c04f
Frederick Ho <frederick.cl.ho@gmail.com>
2019-03-19 22:52:44 +1300

    Create git symlink in vagrant home directory to persistent disk

--- README.md
+++ README.md
@@ -1,17 +1,42 @@
-To Add
+# To Implment
 
-Secondary Disk
-Apt cache packages
-Aliasasls
-
-git
 brew
-terraform
 terragrunt
-terminal
-
-symlink to git
-
-Create a proxy apt host
-
+terraform
+git
+copy secrets
 .vim preferences
+ssh config for git
+Aliases
+  tree
+terminator
+VSCode
+VSCode preferences
+
+# Done
+Secondary Disk
+symlink to git
+Apt cache packages
+
+# Notes
+The secondary disk can be copied prior to destruction.
+
+# Linux Brew
+
+- Install the Linuxbrew dependencies if you have sudo access:
+  Debian, Ubuntu, etc.
+    sudo apt-get install build-essential
+  Fedora, Red Hat, CentOS, etc.
+    sudo yum groupinstall 'Development Tools'
+  See http://linuxbrew.sh/#dependencies for more information.
+- Add Linuxbrew to your ~/.bash_profile by running
+    echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.bash_profile
+    echo 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"' >>~/.bash_profile
+    echo 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"' >>~/.bash_profile
+- Add Linuxbrew to your PATH
+    PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+- We recommend that you install GCC by running:
+    brew install gcc
+- Run `brew help` to get started
+- Further documentation:
+    http://docs.brew.sh
--- provision/provision.bash
+++ provision/provision.bash
@@ -7,6 +7,7 @@ secondary_disk_mountpoint="/persistent"
 # Add SSH key for vagrant user
 grep "$vagrant_ssh_key_comment" /home/vagrant/.ssh/authorized_keys > /dev/null
 if [ $? -ne 0  ]; then
+  echo "Adding $vagrant_ssh_key_comment to vagrant ssh authorized keys"
   cat /provision/vagrant-ssh/freddy_linux_vagrant_id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 fi
 
@@ -29,11 +30,18 @@ if [ $? -ne 0  ]; then
   echo "$secondary_disk_dev  $secondary_disk_mountpoint  ext4  defaults  0 2" >> /etc/fstab
 fi
 
-
 # Apt updates
 cp /provision/apt/archives/*.deb /var/cache/apt/archives/
 apt-get -y update
 
+# Symlink git
+mkdir -p $secondary_disk_mountpoint/vagrant/git
+chown vagrant:vagrant $secondary_disk_mountpoint/vagrant/git
+if [ ! -d /home/vagrant/git ]; then
+  echo "Creating symlink /home/vagrant/git"
+  ln -s $secondary_disk_mountpoint/vagrant/git /home/vagrant/git
+  chown -h vagrant:vagrant /home/vagrant/git
+fi
 
 # Install Brew
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
@@ -42,6 +50,9 @@ apt-get -y update
 
 echo "=== Installing Linux GUI..."
 apt-get -y install lubuntu-core --no-install-recommends
+
+# Copy cached debian packages for reuse in quicker subsequent vagrant recreations  
+cp /var/cache/apt/archives/*.deb /provision/apt/archives/*.deb
 
 echo "======================================================================"
 echo 'If this was your first "vagrant up" then you may need to restart your VM to get your GUI'
